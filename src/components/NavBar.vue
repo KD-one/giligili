93a2913ec2dd07e099a64551ad7323059c0e4d1c
kd <1762495803@qq.com>
2023-07-28 08:47:38 +0800

    完成导航栏菜单的路由功能

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,9 +5,10 @@
   @select="handleSelect"
   background-color="#545c64"
   text-color="#fff"
-  active-text-color="#ffd04b">
-  <el-menu-item index="1">首页</el-menu-item>
-  <el-menu-item index="2">投稿</el-menu-item>
+  active-text-color="#ffd04b"
+  router>
+  <el-menu-item index="/">首页</el-menu-item>
+  <el-menu-item index="/about">投稿</el-menu-item>
 </el-menu>
   </template>
   
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="欢迎来到G站~"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
